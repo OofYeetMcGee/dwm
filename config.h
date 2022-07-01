48acad232a80ddc46dac6eed723f168708584c00
Jared <jaredhenderson@tutanota.com>
2022-06-30 23:33:29 -0600

    Fixed missing comma

--- config.h
+++ config.h
@@ -121,7 +121,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ MODKEY						XK_n,	   togglealttag,   {0} },
+	{ MODKEY,						XK_n,	   togglealttag,   {0} },
 	{ MODKEY,						XK_a,	   shiftview,	   {.i = -1} },
 	{ MODKEY,						XK_s,	   shiftview,	   {.i = +1} },
 	TAGKEYS(                        XK_1,                      0)
